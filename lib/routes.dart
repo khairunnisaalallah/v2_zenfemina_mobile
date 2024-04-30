5793c38128a634dfb533d1e01a1501cab627ac0a
khairunnisaalallah <124749527+khairunnisaalallah@users.noreply.github.com>
2024-04-30 08:38:40 +0700

    add: edit profile design

---
+++ lib/routes.dart
@@ -0,0 +1,45 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:zenfemina_v2/navigation/article.dart';
+import 'package:zenfemina_v2/navigation/dashboard.dart';
+import 'package:zenfemina_v2/navigation/pray.dart';
+import 'package:zenfemina_v2/navigation/profile.dart';
+import 'package:zenfemina_v2/menu/calender.dart';
+import 'package:zenfemina_v2/pages/OTP_page.dart';
+import 'package:zenfemina_v2/pages/cobadisini.dart';
+import 'package:zenfemina_v2/pages/login.dart';
+import 'package:zenfemina_v2/pages/home.dart';
+import 'package:zenfemina_v2/pages/pages.dart';
+import 'package:zenfemina_v2/pages/question1.dart';
+import 'package:zenfemina_v2/pages/question2.dart';
+import 'package:zenfemina_v2/pages/question3.dart';
+import 'package:zenfemina_v2/pages/question4.dart';
+import 'package:zenfemina_v2/pages/register.dart';
+import 'package:zenfemina_v2/pages/splash_screen.dart';
+import 'package:zenfemina_v2/pages/range_date.dart';
+import 'package:zenfemina_v2/pages/profile_menu.dart';
+import 'package:zenfemina_v2/menu/editprofile.dart';
+
+class Routes {
+  static final List<GetPage> pages = [
+    GetPage(name: '/', page: () => home()),
+    GetPage(name: '/login', page: () => LoginPage()),
+    GetPage(name: '/register', page: () => RegisterPage()),
+    GetPage(name: '/article', page: () => articlePage()),
+    GetPage(name: '/dashboard', page: () => DashboardPage()),
+    GetPage(name: '/pray', page: () => prayPage()),
+    GetPage(name: '/profile', page: () => profilePage()),
+    GetPage(name: '/calender', page: () => calenderPage()),
+    GetPage(name: '/otp', page: () => OTPPage()),
+    GetPage(name: '/splash', page: () => SplashScreen()),
+    GetPage(name: '/question1', page: () => question1Page()),
+    GetPage(name: '/question2', page: () => question2Page()),
+    GetPage(name: '/question3', page: () => question3Page()),
+    GetPage(name: '/question3', page: () => question4Page()),
+    GetPage(name: '/otp', page: () => OTPPage()),
+    GetPage(name: '/editprofile', page: () => ProfileView()),
+
+    // Definisikan halaman lain sesuai kebutuhan aplikasi Anda
+  ];
+}
