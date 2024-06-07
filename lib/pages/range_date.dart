00b0b11e3cefb1e0000a92e0b172962829b31701
khairunnisaalallah <124749527+khairunnisaalallah@users.noreply.github.com>
2024-06-08 02:03:17 +0700

    add API calender

--- lib/pages/range_date.dart
+++ lib/pages/range_date.dart
@@ -1,11 +1,7 @@
-// Copyright 2019 Aleksander Woźniak
-// SPDX-License-Identifier: Apache-2.0
-
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-
 import 'package:zenfemina_v2/pages/utils.dart';
-import 'package:zenfemina_v2/shared/shared.dart';
+import 'package:zenfemina_v2/api_repository.dart'; // Pastikan ini adalah path yang benar ke api_repository
 
 class TableRangeExample extends StatefulWidget {
   @override
@@ -22,79 +18,109 @@ class _TableRangeExampleState extends State<TableRangeExample> {
   DateTime? _rangeEnd;
 
   @override
+  void initState() {
+    super.initState();
+    _fetchCycleData();
+  }
+
+  Future<void> _fetchCycleData() async {
+    try {
+      final datahist = await ApiRepository().getCycleData(
+          'hist'); // Pastikan ini adalah cara yang benar untuk memanggil API
+      final dataest = await ApiRepository().getCycleData('est');
+
+      // Log untuk memeriksa struktur data yang diterima
+      print('Data Hist: $datahist');
+      print('Data Est: $dataest');
+
+      final histCycle = datahist['data'];
+      final estCycle = dataest['data'];
+
+      if (histCycle != null) {
+        setState(() {
+          _rangeStart = DateTime.parse(histCycle['start_date']);
+          _rangeEnd = histCycle['end_date'] != null
+              ? DateTime.parse(histCycle['end_date'])
+              : null;
+
+          // Update focusedDay to start of range
+          _focusedDay = _rangeStart!;
+        });
+      } else if (estCycle != null) {
+        setState(() {
+          _rangeStart = DateTime.parse(estCycle['start_date']);
+          _rangeEnd = estCycle['end_date'] != null
+              ? DateTime.parse(estCycle['end_date'])
+              : null;
+
+          // Update focusedDay to start of range
+          _focusedDay = _rangeStart!;
+        });
+      } else {
+        print('No cycle data found in response');
+      }
+    } catch (error) {
+      print('Failed to fetch cycle data: $error');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(height: 40),
+          SizedBox(height: 20),
           TableCalendar(
-            headerStyle: HeaderStyle(
-              headerPadding:
-                  EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
-              titleCentered: true,
-              formatButtonVisible: false,
-              titleTextStyle: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-              ),
-              headerMargin: EdgeInsets.only(bottom: 10),
-            ),
             firstDay: kFirstDay,
             lastDay: kLastDay,
             focusedDay: _focusedDay,
             selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
             rangeStartDay: _rangeStart,
             rangeEndDay: _rangeEnd,
-            calendarStyle: CalendarStyle(
-              rangeHighlightColor:
-                  Color(0xFFDA4256).withOpacity(0.25), //ini warna range date
-              todayDecoration: BoxDecoration(
-                color: Color(0xFFDA4256), // ini warna tgl hari ini
-                shape: BoxShape.circle,
-              ),
-              selectedDecoration: BoxDecoration(
-                color: Color(0xFFDA4256), // ini ga bisa diubah warnnaya
-              ),
-            ),
-            daysOfWeekStyle: DaysOfWeekStyle(
-              weekendStyle: TextStyle(
-                  color: Color(0xFFDA4256)), //ini tulisan sun sama sat
-            ),
             calendarFormat: _calendarFormat,
-            rangeSelectionMode: _rangeSelectionMode =
-                RangeSelectionMode.disabled,
-            // rangeSelectionMode: _rangeSelectionMode,
-            // onDaySelected: (selectedDay, focusedDay) {
-            //   {
-            //     setState(() {
-            //       _selectedDay = selectedDay;
-            //       _focusedDay = focusedDay;
-            //       _rangeStart = null; // Penting untuk membersihkan yang sebelumnya
-            //       _rangeEnd = null;
-            //       _rangeSelectionMode = RangeSelectionMode.toggledOff;
-            //     });
-            //   }
-            // },
-            // onRangeSelected: (start, end, focusedDay) {
-            //   setState(() {
-            //     _selectedDay = null;
-            //     _focusedDay = focusedDay;
-            //     _rangeStart = start;
-            //     _rangeEnd = end;
-            //     _rangeSelectionMode = RangeSelectionMode.toggledOn;
-            //   });
-            // },
-            // onFormatChanged: (format) {
-            //   if (_calendarFormat != format) {
-            //     setState(() {
-            //       _calendarFormat = format;
-            //     });
-            //   }
-            // },
-            // onPageChanged: (focusedDay) {
-            //   _focusedDay = focusedDay;
-            // },
-          ),
+            rangeSelectionMode: _rangeSelectionMode,
+            calendarStyle: CalendarStyle(
+              rangeHighlightColor: Color(0xFFDA4256).withOpacity(0.3),
+              rangeStartDecoration: BoxDecoration(
+                color: Color(0xFFDA4256).withOpacity(1),
+                shape: BoxShape.circle, // atau bentuk lainnya
+              ),
+              rangeEndDecoration: BoxDecoration(
+                color: Color(0xFFDA4256).withOpacity(1),
+                shape: BoxShape.circle, // atau bentuk lainnya
+              ),
+            ),
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                  _rangeStart = null; // Penting untuk membersihkan ini
+                  _rangeEnd = null;
+                  _rangeSelectionMode = RangeSelectionMode.toggledOff;
+                });
+              }
+            },
+            onRangeSelected: (start, end, focusedDay) {
+              setState(() {
+                _selectedDay = null;
+                _focusedDay = focusedDay;
+                _rangeStart = start;
+                _rangeEnd = end;
+                _rangeSelectionMode = RangeSelectionMode.toggledOff;
+              });
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+          )
         ],
       ),
     );
